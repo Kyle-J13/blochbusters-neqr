b2f2a17bb2f4ff689ebebb53053b66d87cc34c2e
ProcrastParker <chrisparker060404@gmail.com>
2021-07-22 16:44:20 -0400

    Started porting 4x4 unit test

--- NEQR Implementation/QiskitTestProject/Qiskit_test.py
+++ NEQR Implementation/QiskitTestProject/Qiskit_test.py
@@ -10,6 +10,8 @@ import QiskitQuantumOperation
 import random
 import math
 
+from typing import List, Sequence
+
 class Qiskit_test(unittest.TestCase):
     def test_2x2(self):
 
@@ -26,14 +28,14 @@ class Qiskit_test(unittest.TestCase):
         #index = QuantumRegister(index_length)
         #intensity = QuantumRegister(greyscale_length)
         #index_measurement = ClassicalRegister(index_length + 8)
-        #intensity_measuremnt = ClassicalRegister(index_length + 8)
-        #circuit = QuantumCircuit(index, intensity, index_measurment, intensity_measurement)
+        #intensity_measurement = ClassicalRegister(index_length + 8)
+        #circuit = QuantumCircuit(index, intensity, index_measurement, intensity_measurement)
 
         correctValues = ["0000000000","0111001000","1001100100","1111111111"]
         resultValues = []
         unique_num = 0
 
-        circuit = QiskitQuantumOperation.operation
+        circuit = QiskitQuantumOperation.operation(_2dArray)
 
 
         simulator = Aer.get_backend('aer_simulator')
@@ -47,29 +49,115 @@ class Qiskit_test(unittest.TestCase):
                 index_state = states[0]
                 intensity_state = states[1]
                 value = index_state+intensity_state
-                print("Found value: " + value)
+                print("Found value:", value)
 
                 if value in resultValues:
                     pass
                 else:
                     resultValues += (index_state+intensity_state)
                     unique_num+=1
-                    print("Value: " + value + " was unique!")
+                    print("Value:", value, "was unique!")
 
                 if unique_num == 4:
                     break
         
-        print("Correct values: " + correctValues)
-        print("Result values: " + resultValues)
+        print("Correct values:", correctValues)
+        print("Result values:", resultValues)
 
         for result in resultValues:
             contains = False
             if result in correctValues:
                 contains = True
             if contains == False:
-                self.fail("The value " + result + " is incorrect")
+                self.fail("The value ", result, " is incorrect")
 
+    def test_4x4(self):
+        _2dArray = [
+            [182, 200 , 1   , 50 ],
+            [255, 75  , 175 , 200],
+            [85 , 170 , 0   , 220],
+            [20 , 245 , 135 , 140]
+        ]
+
+        correctValues = findCorrectValues(_2dArray, 8)
+
+        circuit = QiskitQuantumOperation.operation(_2dArray)
+
+
+# # # # # # # # # # # # # 
+#   Helper Functions    #
+# # # # # # # # # # # # # 
+
+def stringToBoolList(string : str) -> List[bool]:
+    rtn = []
+
+    for l in string:
+        if l == "1":
+            rtn.append(True)
+        else:
+            rtn.append(False)
+
+    return rtn
+
+def boolListToString(binary : List[bool]) -> str:
+    rtn = ""
+
+    for val in binary:
+        if val:
+            rtn += "1"
+        else:
+            rtn += "0"
+
+    return rtn 
+
+def findCorrectValues(_2dArray : List[List[int]], grayLen : int) -> List[str]:
+    indexLen = 2 * int(math.log2(len(_2dArray)))
+
+    correctValues = []
+
+    for row in _2dArray:
+        for col in _2dArray[row]:
+            index = padWithZeros(intToBinaryString(row), indexLen //2)
+            index += padWithZeros(intToBinaryString(col), indexLen // 2)
+
+            grayBinary = padWithZeros(
+                intToBinaryString(_2dArray[row][col]), grayLen
+            )
+
+            correctValues.append(index + grayBinary)
+
+    return correctValues
+
+            
+def intToBinaryString(integer : int) -> str:
+    rtn = ""
+    while integer > 0:
+        rtn += integer % 2
+        integer = integer // 2
+
+    return rtn[::-1]
         
+def padWithZeros(binary : Sequence, length : int, BE=True):
+    # Get the type of the binary representation
+    tp = type(binary)
+
+    # If it's big-endian, flip it
+    if BE:
+        binary = binary[::-1]
+
+    # The difference between the current length and the target length
+    diff = length - len(binary)
+
+    for i in range(diff):
+        # Convert binary 0 into whatever type the 
+        binary += tp(0b0)
+
+    # Flip it back
+    if BE:
+        binary = binary[::-1]
+
+    return binary
+
 
 if __name__ == '__main__':
     unittest.main()
