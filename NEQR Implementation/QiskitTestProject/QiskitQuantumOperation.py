f1a095772750f62e4375300fc9009fd5abf27672
Jocelyn <jocelynjiajie@gmail.com>
2021-07-23 08:19:55 -0700

    still debugging 7/23

--- NEQR Implementation/QiskitTestProject/QiskitQuantumOperation.py
+++ NEQR Implementation/QiskitTestProject/QiskitQuantumOperation.py
@@ -8,6 +8,7 @@ import math
 class ImageProcessing():
     def Operation (grayScaleValues):
         indexLen = 2 * round(math.log(len(grayScaleValues[0]), 2))
+        print(indexLen)
         indexPos = QuantumRegister(indexLen)
         intensity = QuantumRegister(8)
         index_measurement = ClassicalRegister(indexLen)
@@ -18,39 +19,57 @@ class ImageProcessing():
         circuit.h(indexPos)
         for row in range(0, len(grayScaleValues[0])):
             for col in range(0, len(grayScaleValues[0])):
-                NEQRImageProcess(circuit, indexLen, row, col, grayScaleValues[row][col])
+                circuit = ImageProcessing.NEQRImageProcess(indexPos, intensity, index_measurement, intensity_measurement, row, col, grayScaleValues[row][col])
+
+        return circuit
     
-    def NEQRImageProcess (circuit, lengthIndex, row, col, grayScaleValue):
-        grayBinary = convertToBinary(grayScaleValue)
+    def NEQRImageProcess (indexPosition, intensityA, indexM, intensityM, row, col, grayScaleValue):
+        grayBinary = ImageProcessing.convertToBinary(grayScaleValue)
 
-        rowBinary = convertToBinary(row)
-        colBinary = convertToBinary(col)
+        rowBinary = ImageProcessing.convertToBinary(row)
+        colBinary = ImageProcessing.convertToBinary(col)
 
-        if len(rowBinary) < lengthIndex / 2:
-            rowBinary = padWithZeros(rowBinary, lengthIndex / 2)
+        print(len(rowBinary))
+        print(len(colBinary))
+        print(len(indexPosition))
 
-        if len(colBinary) < lengthIndex / 2:
-            colBinary = padWithZeros(colBinary, lengthIndex / 2)
+
+        if len(rowBinary) < len(indexPosition) / 2:
+            rowBinary = ImageProcessing.padWithZeros(rowBinary, len(indexPosition) / 2)
+        #print (rowBinary)
+
+        if len(colBinary) < len(indexPosition) / 2:
+            colBinary = ImageProcessing.padWithZeros(colBinary, len(indexPosition) / 2)
+        #print(colBinary)
 
         if len(grayBinary) < 8:
-            grayBinary = padWithZeros(grayBinary, 8)
+            grayBinary = ImageProcessing.padWithZeros(grayBinary, 8)
 
+
+        print(len(rowBinary))
+        print(len(colBinary))
         indexBinary = rowBinary + colBinary
 
-        if lengthIndex != len(indexBinary):
+        if len(indexPosition) != len(indexBinary):
             print("Fail because binary length not equal to required index length")
+        
+        circuit = QuantumCircuit(indexPosition, intensityA, indexM, intensityM)
 
         for i in range(0, len(indexBinary)):
             if not indexBinary[i]:
-                circuit.x(indexPos[i])
+                print (len(indexBinary)) 
+                print(len(indexPosition))
+                circuit.x(indexPosition[i])
 
         for i in range(0, 8):
             if grayBinary[i]:
-                circuit.cx(indexPos, intensity[i]) # think intensity is the right register? not sure though
+                circuit.cx(indexPosition, intensityA[i]) # think intensity is the right register? not sure though
 
         for i in range(0, len(indexBinary)):
             if not indexBinary[i]:
-                circuit.x(indexPos[i])
+                circuit.x(indexPosition[i])
+        
+        return circuit
 
              
     def convertToBinary (value):
@@ -68,7 +87,7 @@ class ImageProcessing():
     def padWithZeros (binary, targetLen):
         diff = targetLen - len(binary)
 
-        for i in range(0, diff):
+        for i in range(0, int(diff)):
             binary = [False] + binary
 
         return binary
